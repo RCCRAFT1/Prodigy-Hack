db12d73ac58162f36fd38b92e766d1308011e081
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2019-09-20 19:05:41 -0700

    Update Main UI.cpp

--- Main UI.cpp
+++ Main UI.cpp
@@ -3,3 +3,12 @@ set value (0) for id 0
 set kai ui for id 0
 (setvalue)
 for (class) set id to 1
+for (class) set id to 2
+for a set of class, id "0-100"
+set for try 
+if id is not "0-100", do "101-201"
+set for try
+if id is not "101-201", do "202-302"
+set for try
+if id is not "202-302", do "303-403"
+set for try

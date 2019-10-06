57f5d1d7eb59100834e50b47d62a771693e683c8
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2019-10-06 20:36:40 +0000

    Add files via upload

---
+++ Java bash.java
@@ -0,0 +1,22 @@
+use<if>/={PUT BIP HERE}
+weblist
+for use, 
+use <if> supress is not (1038)
+use <if> supress is not (1039)
+use <if> supress is not (1040)
+use <if> supress is not (1041)
+use <if> supress is not (1042)
+use <if> supress is not (1043)
+./bash=/{supress BIP}
+>sudo bash -c bip</
+if BIP is positive, run FILE
+
+FILE:
+(for class reset)  
+supress if (class) is more than (0)
+do not supress if (classBIP) is more than (100000)
+(0)
+
+./bash=/
+
+

58c383cefdc65beef55a6d11617597de964c4220
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2019-10-11 15:36:07 -0700

    Update KAI UI API.js

--- KAI UI API.js
+++ KAI UI API.js
@@ -9,3 +9,6 @@ load kai to main Uint16Array.cpp
 load cc.dll to Unit10Array
 load cc.dll
 if load is not specified, then run depend :cc.dll
+
+loader=/ bash goto </brute>
+  if load is specified, run Main Run.cpp

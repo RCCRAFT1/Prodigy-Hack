16a12895e67e7faaab7bb08fe3cd2cb3b2f772a0
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2019-10-16 00:21:12 +0000

    'Desk Printer' means 'for sudo, bash of httpWEB

---
+++ Desk Printer.rb
@@ -0,0 +1,10 @@
+print ("Hello!")
+print ("welcome to Prodigy hack!")
+print ("I am not with prodigy in any way")
+print ("thanks for downloading!")
+
+kik BIP
+run kik
+plug
+plugin download
+download from https://kik.bashmakers.net/downloads/v9.1.3-DEV

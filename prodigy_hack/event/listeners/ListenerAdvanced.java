11c73c773d208fd04b210af69cc1bb36690310bf
Ryan <43323661+RCCRAFT1@users.noreply.github.com>
2021-11-02 21:15:23 -0700

    fix

---
+++ prodigy_hack/event/listeners/ListenerAdvanced.java
@@ -0,0 +1,53 @@
+package prodigy_hack.event.listeners;
+
+public class ListenerAdvanced {
+	public int count = 0;
+    public boolean yourmom;
+
+    public void attachMechanisim(int count){
+        return;
+    }
+    public boolean byteMechanisimSystemAttachment;
+
+    public boolean systemAttachmentDetect;
+
+    public int getCount() {
+        return count;
+    }
+
+    public ListenerAdvanced(int count, boolean yourmom, boolean byteMechanisimSystemAttachment, boolean systemAttachmentDetect) {
+        super();
+        this.count = count;
+        this.yourmom = yourmom;
+        this.byteMechanisimSystemAttachment = byteMechanisimSystemAttachment;
+        this.systemAttachmentDetect = systemAttachmentDetect;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isYourmom() {
+        return yourmom;
+    }
+
+    public void setYourmom(boolean yourmom) {
+        this.yourmom = yourmom;
+    }
+
+    public boolean isByteMechanisimSystemAttachment() {
+        return byteMechanisimSystemAttachment;
+    }
+
+    public void setByteMechanisimSystemAttachment(boolean byteMechanisimSystemAttachment) {
+        this.byteMechanisimSystemAttachment = byteMechanisimSystemAttachment;
+    }
+
+    public boolean isSystemAttachmentDetect() {
+        return systemAttachmentDetect;
+    }
+
+    public void setSystemAttachmentDetect(boolean systemAttachmentDetect) {
+        this.systemAttachmentDetect = systemAttachmentDetect;
+    }
+}
